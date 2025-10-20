02ff1047d7201a6604e24bd22fc45db0d7704ad6
gabycamps <g.camposm@uniandes.edu.co>
2025-10-19 19:22:35 -0500

    punto 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { Serie } from "./Serie.js";
 import { series } from "./data.js";
 var seriesTbody = document.getElementById('series');
+var tableContainer = document.querySelector(".table-container");
 renderSeriesInTable(series);
 function renderSeriesInTable(series) {
     series.forEach(function (s) {
@@ -8,5 +9,18 @@ function renderSeriesInTable(series) {
         trElement.innerHTML = "<td>".concat(s.id, "</td>\n                            <td>").concat(s.name, "</td>\n                            <td>").concat(s.channel, "</td>\n                            <td>").concat(s.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     });
+    renderAverageSeasons();
+}
+function averageSeasons(series) {
+    var total = 0;
+    series.forEach(function (serie) { return (total += serie.seasons); });
+    return total / series.length;
+}
+function renderAverageSeasons() {
+    var average = averageSeasons(series);
+    var avgElement = document.createElement("p");
+    avgElement.textContent = "Seasons average: ".concat(average);
+    avgElement.classList.add("avg");
+    tableContainer.appendChild(avgElement);
 }
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Serie } from "./Serie.js";
 import { series } from "./data.js";
 
 const seriesTbody: HTMLElement = document.getElementById('series')!;
+const tableContainer: HTMLElement = document.querySelector(".table-container")!;
 
 renderSeriesInTable(series);
 
@@ -15,5 +16,24 @@ function renderSeriesInTable(series: Serie[]): void {
                             <td>${s.seasons}</td>`;
     seriesTbody.appendChild(trElement);
     });
+
+    renderAverageSeasons();
 }
 
+
+
+function averageSeasons(series: any[]): number {
+
+    let total = 0;
+    series.forEach((serie) => (total += serie.seasons));
+    return total/series.length
+}
+
+function renderAverageSeasons(): void {
+
+    const average = averageSeasons(series);
+    const avgElement = document.createElement("p");
+    avgElement.textContent = `Seasons average: ${average}`;
+    avgElement.classList.add("avg");
+    tableContainer.appendChild(avgElement);
+}
